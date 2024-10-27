0278890b86d6c2940f3595a29806da9238a79b60
eushajubaer <eushajubaer028@gmail.com>
2024-10-28 02:48:12 +0600

    Color-Chang

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Heading from './components/Heading'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Heading text={'We Are Digital #Marketing# Agency'} className={'text-4xl font-extrabold font-thin text-center'}/>
+    </>
+  )
+}
+
+export default App
